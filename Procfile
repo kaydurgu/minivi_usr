6d9402221923ced0c9b3576bdcbaa375bf29209f
Khan Dakuev <57888663+kaydurgu@users.noreply.github.com>
2023-04-27 13:32:25 +0300

    mvp

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Minivi.wsgi
---
+++ main/urls.py
@@ -0,0 +1,42 @@
+from django.urls import path,include
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('search',views.search,name='search'),
+    path('category-list',views.category_list,name='category-list'),
+    path('brand-list',views.brand_list,name='brand-list'),
+    path('product-list',views.product_list,name='product-list'),
+    path('category-product-list/<int:cat_id>',views.category_product_list,name='category-product-list'),
+    path('brand-product-list/<int:brand_id>',views.brand_product_list,name='brand-product-list'),
+    path('product/<str:slug>/<int:id>',views.product_detail,name='product_detail'),
+    path('filter-data',views.filter_data,name='filter_data'),
+    path('load-more-data',views.load_more_data,name='load_more_data'),
+    path('accounts/signup',views.signup,name='signup'),
+    path('save-review/<int:pid>',views.save_review, name='save-review'),
+    
+    path('order/<int:id>',views.make_order ,name='make_order'),
+    path('order_details/<int:id>',views.order_details ,name='order_details'),
+
+
+    path('my-dashboard',views.my_dashboard, name='my_dashboard'),
+    path('my-orders',views.my_orders, name='my_orders'),
+    path('my-orders-items/<int:id>',views.my_order_items, name='my_order_items'),
+
+    path('add-wishlist',views.add_wishlist, name='add_wishlist'),
+    path('my-wishlist',views.my_wishlist, name='my_wishlist'),
+
+    path('my-reviews',views.my_reviews, name='my-reviews'),
+
+    path('my-addressbook',views.my_addressbook, name='my-addressbook'),
+    path('add-address',views.save_address, name='add-address'),
+    path('activate-address',views.activate_address, name='activate-address'),
+    path('update-address/<int:id>',views.update_address, name='update-address'),
+    path('edit-profile',views.edit_profile, name='edit-profile'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
